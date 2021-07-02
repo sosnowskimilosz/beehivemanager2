76142986f70e10e3b99e80fe150a3015801db3c9
sosnowskimilosz <sosnowskimilosz@gmail.com>
2021-07-02 15:28:40 +0200

    initial commit

---
+++ src/test/java/com/example/behiveemanagerver2/Behiveemanagerver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.behiveemanagerver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Behiveemanagerver2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
